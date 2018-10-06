d936555dd765c6124e9c0575f60f1fdaf8cdd506
svlentink <sv@lent.ink>
2018-10-06 14:00:56 +0000

    dataset splitting

---
+++ step01.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DATADIR=/tmp/dataset
+
+mkdir -p $DATADIR
+cd $DATADIR
+
+if [ ! -f $DATADIR/*.jsons ]; then
+  read -p "Please specify the path to the dataset (e.g. /home/slentink/geo*.tgz):" TARPATH
+  tar xfz $TARPATH
+  mv $DATADIR/*.jsons totalset.jsons
+fi
+
+head -10 totalset.jsons > first10.jsons
+head -1000 totalset.jsons > first1k.jsons
+ls -alh
+wc -l *
