1161a2051373dd3ad1ea27b635ecedd3b4b20443
svlentink <sv@lent.ink>
2018-10-07 03:52:36 +0000

    init mongo

--- step01.sh
+++ step01.sh
@@ -5,13 +5,14 @@ DATADIR=/tmp/dataset
 mkdir -p $DATADIR
 cd $DATADIR
 
-if [ ! -f $DATADIR/*.jsons ]; then
+if ! [ -f $DATADIR/*.jsons ]; then
   read -p "Please specify the path to the dataset (e.g. /home/slentink/geo*.tgz):" TARPATH
   tar xfz $TARPATH
   mv $DATADIR/*.jsons totalset.jsons
 fi
 
 head -10 totalset.jsons > first10.jsons
-head -1000 totalset.jsons > first1k.jsons
+#head -1000 totalset.jsons > first1k.jsons
+split -l 1000 --numeric-suffixes totalset.jsons splitset
 ls -alh
 wc -l *
--- step02/main.py
+++ step02/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-inpf='/datadir/first10.jsons'
+inp='/datadir/first1k.jsons'
+#inp='/datdair/split*'
 cols2keep= [
 'timestamp_ms', #instead of created_at
 'id_str', #instead of id, to avoid math on it
@@ -10,9 +11,13 @@ cols2keep= [
 
 import numpy as np
 import pandas as pd
+from pymongo import MongoClient
+from pprint import pprint
+from glob import glob
 
 #with open(inpf, 'r') as
 df = pd.read_json(inpf, orient='columns', lines=True)
+dbc = MongoClient('db', 27017)
 
 def remove_columns(df,cols_to_keep,cols_to_remove=[]):
   if len(cols_to_remove) == 0:
@@ -22,6 +27,28 @@ def remove_columns(df,cols_to_keep,cols_to_remove=[]):
   print('Removing',cols_to_remove)
   return df.drop(columns=cols_to_remove)
 
-  
-df2 = remove_columns(df,cols2keep)
+def df2mongo(df,dbc,dbname='dataset'):
+  db = dbc[dbname]
+  ts = len(df)
+  if ts <= 1000:
+    arr = json.loads(df.T.to_json()).values()
+    db.insert_many(arr)
+    return db
+  i=0
+  for r in df.iterrows():
+    db.insert_one(r)
+    i++
+    if i %100 == 0:
+      print('Inserting into Mongo',dbname,i,ts)
+  return db
 
+def parse_inp(inp):
+  fls = glob(inp)
+  i=0
+  tf=len(fls)
+  for f in fls:
+    df = pd.read_json(f, orient='columns', lines=True)
+    cleandf = remove_columns(df,cols2keep)
+    df2mongo(cleandf,dbc)
+    i++
+    print('Parsing',i,tf)
